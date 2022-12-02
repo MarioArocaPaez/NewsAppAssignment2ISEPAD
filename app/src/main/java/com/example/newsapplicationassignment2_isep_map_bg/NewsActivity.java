60a8e42c7b068aed3beea503cd8f1cc7a41d650e
MarioArocaPaez <73229438+MarioArocaPaez@users.noreply.github.com>
2022-12-02 11:36:23 +0100

    Adding API and new activity

---
+++ app/src/main/java/com/example/newsapplicationassignment2_isep_map_bg/NewsActivity.java
@@ -0,0 +1,14 @@
+package com.example.newsapplicationassignment2_isep_map_bg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news);
+    }
+}
