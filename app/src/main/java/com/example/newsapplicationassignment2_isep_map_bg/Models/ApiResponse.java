d2b6c108e34eaba59c54c2c6181363b3040ae5e6
MarioArocaPaez <73229438+MarioArocaPaez@users.noreply.github.com>
2022-12-02 14:07:05 +0100

    Models for Api

---
+++ app/src/main/java/com/example/newsapplicationassignment2_isep_map_bg/Models/ApiResponse.java
@@ -0,0 +1,34 @@
+package com.example.newsapplicationassignment2_isep_map_bg.Models;
+
+import java.util.List;
+
+public class ApiResponse {
+
+    String status;
+    int totalResults;
+    List<Articles> articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public List<Articles> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<Articles> articles) {
+        this.articles = articles;
+    }
+}
