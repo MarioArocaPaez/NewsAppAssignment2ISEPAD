aea9a0511f569f5171d54caaa8334373570778f2
MarioArocaPaez <73229438+MarioArocaPaez@users.noreply.github.com>
2022-12-02 10:47:43 +0100

    First commit

---
+++ app/src/main/java/com/example/newsapplicationassignment2_isep_map_bg/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.newsapplicationassignment2_isep_map_bg;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView name,mail;
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        mail = findViewById(R.id.mail);
+        logout = findViewById(R.id.logOutBut);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if(account != null){
+            String Name = account.getDisplayName();
+            String Mail = account.getEmail();
+
+            name.setText(Name);
+            mail.setText(Mail);
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LogOut();
+            }
+        });
+    }
+
+    private void LogOut() {
+        LoginActivity.gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+    }
+}
