f19fe46a7a8295ae1a6350cdb42818a02131b906
Benja <byguiloff@uc.cl>
2022-12-07 11:41:44 +0100

    create back button branch

--- app/src/main/java/com/example/newsapplicationassignment2_isep_map_bg/DetailsActivity.java
+++ app/src/main/java/com/example/newsapplicationassignment2_isep_map_bg/DetailsActivity.java
@@ -1,10 +1,13 @@
 package com.example.newsapplicationassignment2_isep_map_bg;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -28,6 +31,12 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         title = findViewById(R.id.textTitle);
         author = findViewById(R.id.textAuthor);
@@ -56,5 +65,14 @@ public class DetailsActivity extends AppCompatActivity {
 
         //TODO: feature ver mas
 
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
