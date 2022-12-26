f5a1a636668cad9a0ae6335c5e6396557430b171
MarioArocaPaez <73229438+MarioArocaPaez@users.noreply.github.com>
2022-12-26 20:58:02 +0100

    Add desing

--- app/src/main/java/com/example/newsapplicationassignment2_isep_map_bg/CountryActivity.java
+++ app/src/main/java/com/example/newsapplicationassignment2_isep_map_bg/CountryActivity.java
@@ -32,13 +32,13 @@ public class CountryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country);
 
-        countries = Arrays.asList(new String[]{"United Arab Emirates","Argentina","Austria","Australia","Belgium","Bulgaria","Brazil",
+        countries = Arrays.asList(new String[]{"All","United Arab Emirates","Argentina","Austria","Australia","Belgium","Bulgaria","Brazil",
                 "Canada","Switzerland","China","Colombia","Cuba","Czechia","Germany","Egypt","France","United Kingdom","Greece","Hong Kong",
                 "Hungary","Indonesia","Ireland","Israel","India","Italy","Japan","Korea","Lithuania","Latvia","Morocco","Mexico","Malasya",
                 "Nigeria","Netherlands","Norway","New Zealand","Philippines","Poland","Portugal","Romania","Serbia","Russia","Saudi Arabia",
                 "Sweden","Singapore","Slovenia","Slovakia","Thailand","Turkey","Taiwan","Ukraine","United States","Venezuela","South Africa"});
 
-        codes = Arrays.asList(new String[]{"ae","ar","at","au","be","bg","br","ca","ch","cn","co","cu","cz","de","eg","fr","gb","gr",
+        codes = Arrays.asList(new String[]{"","ae","ar","at","au","be","bg","br","ca","ch","cn","co","cu","cz","de","eg","fr","gb","gr",
                 "hk","hu","id","ie","il","in","it","jp","kr","lt","lv","ma","mx","my","ng","nl","no","nz","ph","pl","pt","ro","rs","ru","sa",
                 "se","sg","si","sk","th","tr","tw","ua","us","ve","za"});
 
